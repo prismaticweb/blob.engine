0bbb547f715e75cb34eed5e4172655c271479019
space-otter <space-otter@visualhackstudio.com>
2023-09-04 15:57:57 +0200

    Introduce engine

--- packages/core/src/data.ts
+++ packages/core/src/data.ts
@@ -1,3 +1,16 @@
+export type Engine = Readonly<{
+  id: string;
+  propertyGraph: PropertyGraph;
+}>;
+
+export function Engine(spec: { id: string; propertyGraph: PropertyGraph }): Engine {
+  const { id, propertyGraph } = spec;
+
+  return Object.freeze({
+    id,
+    propertyGraph,
+  });
+}
 export type Rule = Readonly<{
   constrain: (...args: Array<any>) => boolean;
   id: string;
